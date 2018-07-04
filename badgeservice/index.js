db071f7fd326275aeb0c65e8b184f5dfa66703f1
Brian Park <brian@xparks.net>
2018-07-04 10:44:10 -0700

    badgeservice: cache all badge marker files, and process the Cloud Storage getFiles() before doing a redirect

--- badgeservice/index.js
+++ badgeservice/index.js
@@ -4,20 +4,25 @@
  *
  * The GET handler at
  *
- * https://us-central1-xparks2015.cloudfunctions.net/badge?project=AceButton
+ * https://us-central1-xparks2015.cloudfunctions.net/badge?project={project}
  *
  * looks for the presence of a file in the given 'bucketName', with the form:
  *
  *    - {project}-PASSED
  *    - {project}-FAILED
  *
- * If the PASSED file is detected, then a "passing" badge from shields.io is
- * returned with a 302 redirect. If the FAILED file is detected, a "failure"
- * badge from shields.io is returned.
+ * There are 4 possible badges:
+ *
+ *  - If the PASSED file is detected, then a "passing" badge from shields.io is
+ *    returned with a 302 redirect.
+ *  - If the FAILED file is detected, a "failure" badge from shields.io is
+ *    returned.
+ *  - If both of these marker files are miising, then an "unknown" badge is
+ *    returned.
+ *  - If both of these markers file exist (which shouldn't happen), then
+ *    an "error" badge is returned.
  *
  * Multiple projects are supported using the 'project' query parameter.
- * The list of valid {project} are in the 'projects' variable below.
- * If the project does not match, then an "invalid" badge is returned.
  */
 
 'use strict';
@@ -25,10 +30,6 @@
 // The Google Cloud Storage bucket name used to hold the continuous integration
 // // (CI) test results. NOTE: Change this to your own bucket.
 const bucketName = 'xparks-jenkins';
-
-// List of acceptable projects. Anything that does not match returns
-// an "invalid" badge. NOTE: Change this to your own list of projects.
-const projects = ['AceButton'];
 
 // Number of milliseconds between successive calls to checkPassOrFail().
 const checkIntervalMillis = 1 * 60 * 1000;
@@ -40,53 +41,54 @@ const badgeBaseUrl = 'https://img.shields.io/badge/';
 // Cache of various meta-info related to the CI status of particular project.
 var projectInfos = {};
 
+// Time when the last check was performed.
+var lastCheckedTime = 0;
+
 /**
  * Check for the presence of the {project}-PASSED or {project}-FAILED files
- * in the Google Cloud Storage bucket named {bucketName}.
+ * in the Google Cloud Storage bucket named by bucketName.
  */
-function checkPassOrFail(project) {
-  if (projects.indexOf(project) < 0) {
-    console.log('checkPassOrFail(): Unknown project: ', project);
-    return;
-  }
-  console.log("checkPassOrFail(): project: ", project);
+function updateProjectInfos(files) {
+  console.log('updateProjectInfos(): processing ', files.length, ' files');
+  const re = /(^[^- ]+)-([^- ]+)$/;
+  files.forEach(file => {
+    const m = file.name.match(re);
+    if (m) {
+      const projectName = m[1];
+      projectInfos[projectName] = {
+        passedFound: (m[2] == 'PASSED'),
+        failedFound: (m[3] == 'FAILED')
+      };
+    }
+  });
+}
 
-  var projectInfo = projectInfos[project];
+/**
+ * Return the badge URI fragment of the project using the projectInfos cache.
+ */
+function getUri(project) {
+  if (project == null) {
+    return 'build-invalid-orange.svg';
+  }
+
+  const projectInfo = projectInfos[project];
   if (projectInfo == null) {
-    console.log('checkPassOrFail(): projectInfo not found');
-    return;
+    return 'build-unknown-lightgrey.svg';
+  } else {
+    if (projectInfo.passedFound) {
+      if (projectInfo.failedFound) {
+        return 'build-error-yellow.svg';
+      } else {
+        return 'build-passing-brightgreen.svg';
+      }
+    } else {
+      if (projectInfo.failedFound) {
+        return 'build-failure-brightred.svg';
+      } else {
+        return 'build-unknown-lightgrey.svg';
+      }
+    }
   }
-
-	const Storage = require('@google-cloud/storage');
-	const storage = new Storage();
-
-  storage
-    .bucket(bucketName)
-    .file('/' + project + '-PASSED')
-    .getMetadata()
-    .then(results => {
-      const metadata = results[0];
-      projectInfo.passedFound = true;
-      console.log('checkPassOrFail(): PASSED found');
-    })
-    .catch(err => {
-      projectInfo.passedFound = false;
-      console.log('checkPassOrFail(): PASSED not found');
-    });
-
-  storage
-    .bucket(bucketName)
-    .file('/' + project + '-FAILED')
-    .getMetadata()
-    .then(results => {
-      const metadata = results[0];
-      projectInfo.failedFound = true;
-      console.log('checkPassOrFail(): FAILED found');
-    })
-    .catch(err => {
-      projectInfo.failedFound = false;
-      console.log('checkPassOrFail(): FAILED not found');
-    });
 }
 
 /**
@@ -100,48 +102,34 @@ function checkPassOrFail(project) {
 exports.badge = (req, res) => {
   const project = req.query.project;
   if (project == null) {
-    console.log('badge(): Missing project parameter');
     res.redirect(badgeBaseUrl + 'build-invalid-orange.svg');
     return;
   }
-  if (projects.indexOf(project) < 0) {
-    console.log('badge(): Invalid project: ', project);
-    res.redirect(badgeBaseUrl + 'build-invalid-orange.svg');
+  console.log('badge(): Processing project: ', project);
+
+  // Use the cache if it was updated recently.
+  const nowMillis = new Date().getTime();
+  if (nowMillis - lastCheckedTime <= checkIntervalMillis) {
+    const uri = getUri(project);
+    res.redirect(badgeBaseUrl + uri);
     return;
   }
-  console.log("badge(): Processing project: ", project);
 
-  var projectInfo = projectInfos[project];
-  if (projectInfo == null) {
-    projectInfo = {
-      passedFound: false,
-      failedFound: false,
-      lastCheckedTime: 0
-    };
-    projectInfos[project] = projectInfo;
-  }
+	const Storage = require('@google-cloud/storage');
+	const storage = new Storage();
 
-  // Schedule asychronous check against the Cloud Storage.
-  var nowMillis = new Date().getTime();
-  if (nowMillis - projectInfo.lastCheckedTime > checkIntervalMillis) {
-    checkPassOrFail(project);
-    projectInfo.lastCheckedTime = nowMillis;
-  }
-
-  var uri;
-  if (projectInfo.passedFound) {
-    if (projectInfo.failedFound) {
-      uri = 'build-error-yellow.svg';
-    } else {
-      uri = 'build-passing-brightgreen.svg';
-    }
-  } else {
-    if (projectInfo.failedFound) {
-      uri = 'build-failure-brightred.svg';
-    } else {
-      uri = 'build-unknown-lightgrey.svg';
-    }
-  }
-  
-  res.redirect(badgeBaseUrl + uri);
+  // Update the cache, then return the results.
+  return storage
+      .bucket(bucketName)
+      .getFiles()
+      .then(results => {
+        const files = results[0];
+        updateProjectInfos(files);
+        lastCheckedTime = nowMillis;
+        const uri = getUri(project);
+        res.redirect(badgeBaseUrl + uri);
+      })
+      .catch(err => {
+        console.error('ERROR: ', err);
+      });
 };
